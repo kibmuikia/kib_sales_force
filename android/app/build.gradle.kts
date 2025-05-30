722763fdee4fa90e64a943bb8f9e0694c5d43c85
Allan Muikia <31766430+kibmuikia@users.noreply.github.com>
2025-05-30 13:22:58 +0300

    minor android config update.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,6 +9,7 @@ android {
     namespace = "dev.kib.kib_sales_force"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
