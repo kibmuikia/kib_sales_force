b0ff684e470bc51ff98f5ff5e19573ea44dd6aa8
Allan Muikia <31766430+kibmuikia@users.noreply.github.com>
2025-05-30 13:06:58 +0300

    setup project

---
+++ README.md
@@ -0,0 +1,7 @@
+# kib_sales_force
+
+Visits Tracker for RTM Sales Force Automation
+
+## Getting Started
+
+To be appropriately populated.
---
+++ android/app/src/main/kotlin/dev/kib/kib_sales_force/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.kib.kib_sales_force
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
